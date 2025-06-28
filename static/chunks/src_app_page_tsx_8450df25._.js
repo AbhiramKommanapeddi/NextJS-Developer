8c667240d2a54ed4f8358751cd8fa2c6f68177e4
Abhiram Kommanapeddi <21A81A0629@sves.org.in>
2025-06-28 14:41:50 +0530

    Add files via upload

---
+++ static/chunks/src_app_page_tsx_8450df25._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_components_616bc4da._.js",
+  "static/chunks/node_modules_7df31c4f._.js"
+],
+    source: "dynamic"
+});
